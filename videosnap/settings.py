8360bb5b92dc2aa2ccbfc1cd6ff949fe361074bf
manikanta7-ART <tammanamanikantamanikanta@gmail.com>
2026-09-04 19:10:59 +0530

    commit

--- videosnap/settings.py
+++ videosnap/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'videosnap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.larnjomqcunkpprnsmqa',
-        'PASSWORD': '@Manikanta2004',
-        'HOST': 'aws-0-ap-southeast-2.pooler.supabase.com',
-        'PORT': '6543',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
